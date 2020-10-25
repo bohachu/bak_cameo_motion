0edd86dcdc9c39c745ec5bb832f542d7f29e66fe
bohachu <cbh@cameo.tw>
2020-10-25 16:07:36 +0000

    ok

--- .ipynb_checkpoints/install-checkpoint.sh
+++ .ipynb_checkpoints/install-checkpoint.sh
@@ -24,39 +24,12 @@
 
 ### voila jupyterlab
 #sudo apt-get -y install npm
-#pip3 install voila
-#jupyter labextension install @jupyter-voila/jupyterlab-preview
-#sudo chmod 777 /usr/
-#jupyter serverextension enable voila --sys-prefix
+# pip3 install voila
+# jupyter labextension install @jupyter-voila/jupyterlab-preview
+# sudo chmod 777 /usr/
+# jupyter serverextension enable voila --sys-prefix
 
 ### jupyterlab julia 
 #julia -e 'import Pkg; Pkg.add("IJulia"); Pkg.build("IJulia"); using IJulia; notebook(detached=true);'
 
-### jupyterlab server extension example
-#cd ~
-#pip3 install --user cookiecutter
-### 要看這邊的文章做相關設定 https://github.com/jupyterlab/extension-examples/tree/master/advanced/server-extension
-# cookiecutter https://github.com/jupyterlab/extension-cookiecutter-ts
-# jlpm add @jupyterlab/services
-# jlpm add @jupyterlab/coreutils
-# sudo chmod 777 /usr/local/lib/python3.7/dist-packages/
-# cd ~/cameo_motion_extension/
-# pip3 install -e .
-# jupyter serverextension enable --py cameo_motion_extension
-
-# # Install server extension in editable mode
-cd ~/cameo_motion_extension/
-pip3 install -e .
-# # Register server extension
-jupyter serverextension enable --py cameo_motion_extension
-# # Install dependencies
-# jlpm
-# # Build Typescript source
-# jlpm build
-# # Install your development version of the extension with JupyterLab
-jupyter labextension install .
-# # Rebuild Typescript source after making changes
-# jlpm build
-# # Rebuild JupyterLab after making any changes
-jupyter lab build
 
