b22fb521ab2ced76ec889c773cd285a02caf6c82
Yueh-Cheng Chang <ycchang.pmp@gmail.com>
2020-12-10 04:56:48 +0800

    nginx

--- sh/install_server.sh
+++ sh/install_server.sh
@@ -19,7 +19,7 @@ sudo python3 -m venv /opt/jupyterhub/
 sudo /opt/jupyterhub/bin/python3 -m pip install wheel
 sudo /opt/jupyterhub/bin/python3 -m pip install jupyterhub jupyterlab
 sudo /opt/jupyterhub/bin/python3 -m pip install ipywidgets
-sudo /opt/jupyterhub/bin/python3 -m pip install keplergl opencv-python
+# sudo /opt/jupyterhub/bin/python3 -m pip install keplergl opencv-python
 
 sudo apt install -y nodejs npm curl wget sudo cron joe nano python3-dev \
    software-properties-common apt-utils ffmpeg libssl1.1 libssl-dev libxtst6 xvfb xdotool wmctrl cmake \
@@ -39,15 +39,16 @@ sudo mkdir -p /opt/jupyterhub/etc/jupyterhub/
 # cd /opt/jupyterhub/etc/jupyterhub/
 
 # sudo /opt/jupyterhub/bin/jupyterhub --generate-config
-sudo cp jupyterhub_config.py /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py
-
-sudo cp jupyterhub.service /opt/jupyterhub/etc/systemd/jupyterhub.service
+sudo cp ~/cameo_motion/sh/jupyterhub_config.py /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py
+sudo chmod a+x /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py
+sudo mkdir -p /opt/jupyterhub/etc/systemd
+sudo cp ~/cameo_motion/sh/jupyterhub.service /opt/jupyterhub/etc/systemd/jupyterhub.service
 sudo ln -s /opt/jupyterhub/etc/systemd/jupyterhub.service /etc/systemd/system/jupyterhub.service
-
+sudo chmod a+x /opt/jupyterhub/etc/systemd/jupyterhub.service
 
 sudo curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | sudo gpg --dearmor > conda.gpg
 sudo install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee /etc/apt/sources.list.d/conda.list
+sudo echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee /etc/apt/sources.list.d/conda.list
 
 sudo apt update && sudo apt install conda -y
 # 會將conda安裝在 /opt/conda/; 指令會在 /opt/conda/bin/conda
@@ -58,7 +59,7 @@ sudo /opt/conda/bin/conda install -c conda-forge -y 'conda-build' && \
     sudo /opt/conda/bin/conda config --prepend channels conda-forge
 
 # Install a default conda environment for all users
-sudo mkdir /opt/conda/envs/
+sudo mkdir -p /opt/conda/envs/
 sudo chmod -R a+w /opt/conda/ && \
     sudo chown -R root:users /opt/conda && \
     sudo chmod g+s /opt/conda
@@ -93,41 +94,40 @@ sudo /opt/conda/bin/conda create --prefix /opt/conda/envs/python python=3.8 \
 sudo /opt/conda/bin/conda build purge-all && \
     sudo /opt/conda/bin/conda clean --all -f -y && \
     sudo rm -fvR /opt/conda/pkgs/*
+sudo /opt/conda/envs/python/bin/
+sudo /opt/conda/envs/python/bin/pip3 install keplergl opencv-python --no-cache
 
-
-sudo jupyter serverextension enable --py jupyterlab --sys-prefix && \
-    sodo jupyter serverextension enable voila --sys-prefix && \
-    sodo jupyter nbextension install --py widgetsnbextension --sys-prefix && \
-    sudo jupyter nbextension enable widgetsnbextension --py --sys-prefix && \
-    sudo export NODE_OPTIONS=--max-old-space-size=4096 && \
-    sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build && \
+export NODE_OPTIONS=--max-old-space-size=4096 && \
+    sudo /opt/conda/envs/python/bin/jupyter serverextension enable --py jupyterlab --sys-prefix && \
+    sodo /opt/conda/envs/python/bin/jupyter serverextension enable voila --sys-prefix && \
+    sodo /opt/conda/envs/python/bin/jupyter nbextension install --py widgetsnbextension --sys-prefix && \
+    sudo /opt/conda/envs/python/bin/jupyter nbextension enable widgetsnbextension --py --sys-prefix && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager keplergl-jupyter --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install @jupyter-widgets/jupyterlab-sidecar --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install @jupyterlab/geojson-extension --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install jupyter-matplotlib --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install spreadsheet-editor --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter labextension install @jupyter-voila/jupyterlab-preview --no-build && \
+    sudo /opt/conda/envs/python/bin/jupyter lab build && \
+    unset NODE_OPTIONS 
     # jupyter labextension install jupyterlab-plotly@4.6.0 --no-build && \
     # jupyter labextension install plotlywidget@4.6.0 --no-build && \
-    sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager keplergl-jupyter --no-build && \
-    sudo jupyter labextension install @jupyter-widgets/jupyterlab-sidecar --no-build && \
-    sudo jupyter labextension install @jupyterlab/geojson-extension --no-build && \
-    sudo jupyter labextension install jupyter-matplotlib --no-build && \
-    sudo jupyter labextension install spreadsheet-editor --no-build && \
-    sudo jupyter labextension install @jupyter-voila/jupyterlab-preview --no-build && \
-    sudo jupyter lab build && \
-    sudo unset NODE_OPTIONS 
 
 
-sudo rm -rf /var/lib/apt/lists/*
+# sudo rm -rf /var/lib/apt/lists/*
 
-# 啟動jupyterhub服務
-sudo systemctl daemon-reload
-sudo systemctl enable jupyterhub.service
-sudo systemctl start jupyterhub.service
+# 啟動jupyterhub服務; 需要重開機才會生效
+# sudo systemctl daemon-reload
+# sudo systemctl enable jupyterhub.service
+# sudo systemctl start jupyterhub.service
 
 
 # 共享目錄設定
 sudo groupadd analysts
-sudo mkdir /srv/data/share_data_analysts
-
+sudo mkdir -p /srv/data/share_data_analysts
 sudo chown -R cameo:analysts /srv/data/share_data_analysts
-
-sudo chmod -R 770 /srv/data/_share_data_analyst
+sudo chmod -R 770 /srv/data/share_data_analysts
 
 # setfacl only works in native linux; not working for WSL 
 # Granting permission for a group named "analysts" would look something like this:
@@ -138,28 +138,62 @@ sudo setfacl -R -m d:o::r /srv/data/share_data_analysts
 sudo setfacl -R -m d:mask:rwx /srv/data/share_data_analysts
 
 
-# 設定靜態網頁檔案
-sudo mkdir /var/www/html/
-sudo cp ../src/* /var/www/html/
+
+
 
 # TODO www需要讓特定使用者(如admin group)可以寫入 但是analysts不能寫入
 
 # TODO jupyterhub_config.py 需要設定建立新使用者時, 自動加入group以及連結上述資料夾到home目錄中
 
+# nginx 安裝啟動設定
+# sudo add-apt-repository ppa:nginx/stable -y
+sudo apt update
+sudo apt install nginx -y
+# cd ~/cameo_motion/sh
+sudo mkdir -p /etc/nginx/sites-enabled/cameo.tw
+sudo cp ~/cameo_motion/sh/nginx_http.conf /etc/nginx/sites-enabled/cameo.tw
+sudo cp ~/cameo_motion/sh/htpasswd /etc/nginx/htpasswd
+# sudo /etc/init.d/nginx reload
+sudo systemctl stop nginx
 
+# 設定靜態網頁檔案
+sudo mkdir -p /var/www/cameo.tw/html/
+sudo cp ../src/* /var/www/cameo.tw/html/
+sudo chown -R $USER:$USER /var/www/cameo.tw/html/
+sudo chmod -R 755 /var/www/cameo.tw/html/
+sudo ln -s /etc/nginx/sites-available/cameo.tw /etc/nginx/sites-enabled/
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 ## install julia
 cd ~
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz
 tar xvfz julia-1.5.3-linux-x86_64.tar.gz
-cd /usr/local/bin/
-sudo ln -s ~/julia-1.5.3/bin/julia julia
+# cd /usr/local/bin/
+sudo ln -s ~/julia-1.5.3/bin/julia /usr/local/bin/julia
+
+source ~/.bashrc
+## jupyterlab julia 
+/usr/local/bin/julia -e 'import Pkg; Pkg.add("IJulia"); Pkg.build("IJulia"); using IJulia; notebook(detached=true);'
+
+## install julia genie
+/usr/local/bin/julia -e 'import Pkg; Pkg.add("PackageCompiler");using PackageCompiler;Pkg.add("Genie");@time using Genie;@time PackageCompiler.create_sysimage(:Genie; replace_default=true)'
+
+## interact.jl
+/usr/local/bin/julia -e 'using Pkg;Pkg.add("Interact");Pkg.add("IJulia");Pkg.add("WebIO")'
+/usr/local/bin/julia -e 'using WebIO; using Interact; WebIO.install_jupyter_labextension();'
+
+## github
+git config --global user.email "ycchang.pmp@cameo.tw"
+git config --global user.name "austinyuch"
+git config --global credential.helper cache
+git config --global credential.helper store
 
 ## deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 cd /usr/local/bin/
-sudo ln -s /home/bohachu/.deno/bin/deno deno
+sudo ln -s /home/cameo/.deno/bin/deno deno
 
 
-# nginx 安裝啟動設定
-
+# setting firewall
